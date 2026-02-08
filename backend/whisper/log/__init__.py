b7a0968ca00fd6267895201fb95086c2665d5c64
Ivan Gorskiy <gorskii241152003@gmail.com>
2026-02-08 23:46:19 +0300

    sdfsf

---
+++ backend/whisper/log/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+logger = logging.getLogger("stt")
+logger.setLevel(logging.INFO)
+
+if not logger.handlers:
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(handler)
---
+++ backend/whisper/models/__init__.py
@@ -0,0 +1,10 @@
+
+"""STT model enums"""
+from enum import Enum
+
+class GroqAudioModels(str, Enum):
+    WHISPER_LARGE_V3_TURBO = "whisper-large-v3-turbo"
+
+class STTModels(str, Enum):
+    WHISPER_LARGE_V3_TURBO = "whisper-large-v3-turbo"
+
